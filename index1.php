0e12c428fc972442c081c282094e002682f9086c
aswinrprasad <aswinrprasad@gmail.com>
2019-04-10 21:28:01 +0530

    Required for login testing

---
+++ index1.php
@@ -0,0 +1,45 @@
+<HTML>
+<HEAD>
+<TITLE>Welcome to ASEeats Online!</TITLE>
+<style type="text/css">
+@import url(style.css);
+   a:link {color: #ffffff}
+   a:visited {color: #ffffff}
+   a:hover {color: #ffffff}
+   a:active {color: #ffffff}
+</style>
+</HEAD>
+<BODY background="background1.jpg">
+<?php include("header.php"); ?>
+<FONT size="5" color="white">
+<SECTION align="center"><A HREF="index.php"><IMG SRC="logo.png" alt="Home" id="logo"></IMG></A></SECTION>
+<SECTION>
+<MAIN>
+<B><P>Welcome, you are logged in. 
+        <br />
+		Thank you for choosing us.
+		<br /></P></B></FONT>
+<SECTION align="center"><IMG src="clickhere.gif" width="100" height="50"></IMG></SECTION>
+<div id="form-content">
+	<div class="welcome" style="display: block;">
+		<center><a href="order.php" style="color:#ffffff">Place an order</a></center>
+		<center><a href="changepw.php" style="color:#ffffff">Change password</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+		<a href="deact.php" style="color:#ffffff">Deactivate your account</a></center><br /><br/>
+		<section align="right"><a href="log.php?ac=logout" style="color:#ffffff">Logout</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</section>
+		<script>
+		if (window.performance && window.performance.navigation.type == window.performance.navigation.TYPE_BACK_FORWARD) 
+        	{
+        		window.location.href='log.php';
+    			alert("You cannot go back to the home page! Login if you want to!");
+			}
+		</script>
+	</div>	
+</div>
+</SECTION>
+</MAIN><BR><HR width="1000">
+<FOOTER >
+<FONT size="2" color="white">
+Save time, be smart!</FONT>
+</FOOTER>
+</BODY>
+</HTML>
---
+++ log.php
@@ -0,0 +1,181 @@
+<?php
+session_start();
+$_SESSION['message']='';
+$_SESSION['email']=null;
+
+if($_SESSION['email']==null) 
+{
+include_once("log.php");	
+if ($_SERVER['REQUEST_METHOD'] == 'POST')
+{
+	$conn =new mysqli('localhost','root','newhorizon','ACCASE');
+	$email=$conn->real_escape_string($_POST['email']);
+	$password=$conn->real_escape_string($_POST['pw']);
+	$res= $conn->query("SELECT * FROM php_users_login WHERE email='$email'");
+	if( $res->num_rows <= 0 ){
+	$_SESSION['message']="User does not exist.";
+	} 
+else
+{	
+	$user = $res->fetch_assoc();
+    if($password===$user['password']){
+        $_SESSION['email'] = $user['email'];
+        $_SESSION['logged_in'] = true;
+        header("location:index1.php ");
+        exit();
+    }
+	else
+	{
+		$_SESSION['message']="You have entered the wrong password. Try again.";
+	}
+}
+}
+}
+else
+{
+	header("location:log.php");
+	alert("Already logged into another account, logout first!");
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>ASEeats v1</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+<!--===============================================================================================-->	
+	<link rel="icon" type="image/png" href="images/icons/favicon.ico"/>
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="fonts/Linearicons-Free-v1.0.0/icon-font.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/animate/animate.css">
+<!--===============================================================================================-->	
+	<link rel="stylesheet" type="text/css" href="vendor/css-hamburgers/hamburgers.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/animsition/css/animsition.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/select2/select2.min.css">
+<!--===============================================================================================-->	
+	<link rel="stylesheet" type="text/css" href="vendor/daterangepicker/daterangepicker.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="css/util.css">
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+<!--===============================================================================================-->
+<SCRIPT type="text/javascript">
+function validate()
+{
+	var EmailId=document.getElementById("email");
+	var pw=document.getElementById("pw");
+	var alphaExp = /^[a-zA-Z]+$/;
+	var atpos = EmailId.value.indexOf("@");
+    var dotpos = EmailId.value.lastIndexOf(".");
+
+	if (atpos<1 || dotpos<atpos+2 || dotpos+2>=EmailId.value.length) 
+	{
+        alert("Enter valid email-ID");
+		EmailId.focus();
+        return false;
+   	}
+
+
+	if(EmailId.value==null || EmailId.value=="")
+	{
+		lname.focus();
+		alert("Enter valid email-ID");
+		return false;
+	}
+
+	if(pw.value.length< 8 )
+	{
+		alert("Password Invalid");
+		pw.focus();
+		return false;
+	}
+
+
+	if (confirm("Do you want to submit your details?") == true) {} 
+	else 
+	{
+       return false;
+    }
+	return true;
+}
+</SCRIPT>
+</head>
+<body>
+	
+	<div class="limiter">
+		<div class="container-login100">
+			<div class="wrap-login100">
+				<form class="login100-form validate-form" method="post" action="log.php" onsubmit="return validate()">
+					<span class="login100-form-title p-b-34">
+						Login to ASE Eats
+					</span>
+					<div class="alert alert-error"><?=$_SESSION['message'] ?></div>
+					<div class="wrap-input100 rs1-wrap-input100 validate-input m-b-20" data-validate="Type user name">
+						<input id="email" class="input100" type="text" name="email" placeholder="College mail ID">
+						<span class="focus-input100"></span>
+					</div>
+					<div class="wrap-input100 rs2-wrap-input100 validate-input m-b-20" data-validate="Type password">
+						<input class="input100" type="password" name="pw" id="pw" placeholder="Password">
+						<span class="focus-input100"></span>
+					</div>
+					
+					<div class="container-login100-form-btn">
+						<button class="login100-form-btn">Sign in</button>
+					</div>
+
+					<div class="w-full text-center p-t-27 p-b-239">
+						<span class="txt1">
+							Forgot
+						</span>
+
+						<a href="#" class="txt2">
+							User name / password?
+						</a>
+					</div>
+
+					<div class="w-full text-center">
+						<a href="register.php" class="txt3">
+							Sign Up
+						</a>
+					</div>
+					</form>
+				<div class="login100-more" style="background-image: url('images/bg-01.jpg');"></div>
+			</div>
+		</div>
+	</div>
+	
+
+	<div id="dropDownSelect1"></div>
+	
+<!--===============================================================================================-->
+	<script src="vendor/jquery/jquery-3.2.1.min.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/animsition/js/animsition.min.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/bootstrap/js/popper.js"></script>
+	<script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/select2/select2.min.js"></script>
+	<script>
+		$(".selection-2").select2({
+			minimumResultsForSearch: 20,
+			dropdownParent: $('#dropDownSelect1')
+		});
+	</script>
+<!--===============================================================================================-->
+	<script src="vendor/daterangepicker/moment.min.js"></script>
+	<script src="vendor/daterangepicker/daterangepicker.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/countdowntime/countdowntime.js"></script>
+<!--===============================================================================================-->
+	<script src="js/main.js"></script>
+
+</body>
+</html>
